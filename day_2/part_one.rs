2f8d693ab12fa0dc313ed7194eb1fd5747e16e16
Alex Gladyshev <alex@lifter.ai>
2023-12-02 16:01:11 +0300

    Find if game is valid, calc sum

--- day_2/part_one.rs
+++ day_2/part_one.rs
@@ -3,31 +3,41 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() -> std::io::Result<()> {
+    let mut result = 0;
     let mut bag = HashMap::new();
-    bag.insert("red", 12);
-    bag.insert("green", 13);
-    bag.insert("blue", 14);
+    bag.insert("red".to_string(), 12);
+    bag.insert("green".to_string(), 13);
+    bag.insert("blue".to_string(), 14);
     let file = File::open("input")?;
     let reader = BufReader::new(file);
     for line_result in reader.lines() {
         let line = line_result?;
         let game_data: Vec<&str> = line.split(":").collect();
-        let game_number = getInt(game_data[0]).expect("No game number found");
+        let game_number = get_int(game_data[0]).expect("No game number found");
         let rounds: Vec<&str> = game_data[1].split(";").collect();
-        for round in rounds {
-            let entries: Vec<&str> = round.split(",").collect();
-            for entry in entries {
-                let value = getInt(entry).expect("Missing cube number");
-                let key = getKey(entry, &bag).expect("Missing cube color");
-                println!("{} {}", key, value);
-            }
+        if is_valid_game(rounds, &bag) {
+            result += game_number;
         }
-        break;
     }
+    println!("{}", result);
     Ok(())
 }
 
-fn getKey(str: &str, map: &HashMap<&str, u32>) -> Option<String> {
+fn is_valid_game(rounds: Vec<&str>, bag: &HashMap<String, u32>) -> bool {
+    for round in rounds {
+        let entries: Vec<&str> = round.split(",").collect();
+        for entry in entries {
+            let value = get_int(entry).expect("Missing cube number");
+            let key = get_key(entry, bag).expect("Missing cube color");
+            if value > *bag.get(&key).expect("Key not found") {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn get_key(str: &str, map: &HashMap<String, u32>) -> Option<String> {
     for key in map.keys() {
         if str.contains(key) {
             return Some(key.to_string());
@@ -36,7 +46,7 @@ fn getKey(str: &str, map: &HashMap<&str, u32>) -> Option<String> {
     None
 }
 
-fn getInt(str: &str) -> Option<u32> {
+fn get_int(str: &str) -> Option<u32> {
     let mut digits = String::new();
     for char in str.chars() {
         if char.is_digit(10) {
