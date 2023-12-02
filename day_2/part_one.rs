f271233ba2c3ac0bf6b5485f4cb6b7fb0dee720b
Alex Gladyshev <alex@lifter.ai>
2023-12-02 15:34:00 +0300

    Get cube color and number of cubes

--- day_2/part_one.rs
+++ day_2/part_one.rs
@@ -3,6 +3,10 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() -> std::io::Result<()> {
+    let mut bag = HashMap::new();
+    bag.insert("red", 12);
+    bag.insert("green", 13);
+    bag.insert("blue", 14);
     let file = File::open("input")?;
     let reader = BufReader::new(file);
     for line_result in reader.lines() {
@@ -10,9 +14,26 @@ fn main() -> std::io::Result<()> {
         let game_data: Vec<&str> = line.split(":").collect();
         let game_number = getInt(game_data[0]).expect("No game number found");
         let rounds: Vec<&str> = game_data[1].split(";").collect();
-        println!("{}", game_number);
+        for round in rounds {
+            let entries: Vec<&str> = round.split(",").collect();
+            for entry in entries {
+                let value = getInt(entry).expect("Missing cube number");
+                let key = getKey(entry, &bag).expect("Missing cube color");
+                println!("{} {}", key, value);
+            }
+        }
+        break;
     }
     Ok(())
+}
+
+fn getKey(str: &str, map: &HashMap<&str, u32>) -> Option<String> {
+    for key in map.keys() {
+        if str.contains(key) {
+            return Some(key.to_string());
+        }
+    }
+    None
 }
 
 fn getInt(str: &str) -> Option<u32> {
