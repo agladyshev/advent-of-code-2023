08495fbde4cbe02941f2d37c278b46457450f9a3
Alex Gladyshev <alex@lifter.ai>
2023-12-15 13:14:32 +0300

    13:refactor

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -6,51 +6,52 @@ use std::io::{BufRead, BufReader};
 fn main() -> std::io::Result<()> {
     let file = File::open("test.txt")?;
     let reader = BufReader::new(file);
-    let mut row_caches: HashMap<usize, u64> = HashMap::new();
-    let mut column_caches: HashMap<usize, u64> = HashMap::new();
     let mut rows: Vec<String> = Vec::new();
-    let mut columns: Vec<Vec<char>> = Vec::new();
+    let mut columns: Vec<String> = Vec::new();
+    let mut column_chars: Vec<Vec<char>> = Vec::new();
     for (n, line_result) in reader.lines().enumerate() {
         let line = line_result.expect("Bad line");
         if n == 0 {
             let columns_count = line.len();
             for _i in 0..columns_count {
-                columns.push(Vec::new());
+                column_chars.push(Vec::new());
             }
         }
         let first_char = line.chars().next();
         match first_char {
             Some('.') | Some('#') => {
                 for (i, char) in line.chars().enumerate() {
-                    columns[i].push(char);
+                    column_chars[i].push(char);
                 }
                 rows.push(line);
             }
             None | _ => {
-                {
-                    let mut i = 0;
-                    for str in &rows {
-                        row_caches.insert(i, to_hash(str));
-                        i += 1;
-                    }
+                for chars in &column_chars {
+                    let str: String = chars.into_iter().collect();
+                    columns.push(str);
                 }
-                {
-                    let mut i = 0;
-                    for column in &columns {
-                        let str: String = column.into_iter().collect();
-                        column_caches.insert(i, to_hash(&str));
-                        i += 1;
-                    }
-                }
+                let row_caches: HashMap<usize, u64> = populate_hashmap(&rows);
+                let column_caches: HashMap<usize, u64> = populate_hashmap(&columns);
+                let mut row_above = 0;
+                let mut columns_left = 0;
                 println!("{:?}", row_caches);
                 println!("{:?}", column_caches);
                 //empty line
                 //process mirrors
-                //
             }
         }
     }
     Ok(())
+}
+
+fn populate_hashmap(strs: &Vec<String>) -> HashMap<usize, u64> {
+    let mut hashmap: HashMap<usize, u64> = HashMap::new();
+    let mut i = 0;
+    for str in strs {
+        hashmap.insert(i, to_hash(str));
+        i += 1;
+    }
+    hashmap
 }
 
 fn to_hash(str: &str) -> u64 {
