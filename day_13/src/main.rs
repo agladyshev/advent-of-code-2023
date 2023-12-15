f387392110dd04082ed84db65731b2ed2ac89a57
Alex Gladyshev <alex@lifter.ai>
2023-12-15 13:57:31 +0300

    13: find mirror in rows

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use std::collections::hash_map::{DefaultHasher, HashMap};
 use std::fs::File;
 use std::hash::{Hash, Hasher};
@@ -11,7 +12,8 @@ fn main() -> std::io::Result<()> {
     let mut column_chars: Vec<Vec<char>> = Vec::new();
     for (n, line_result) in reader.lines().enumerate() {
         let line = line_result.expect("Bad line");
-        if n == 0 {
+        println!("Line {} {}", n, &line);
+        if column_chars.is_empty() {
             let columns_count = line.len();
             for _i in 0..columns_count {
                 column_chars.push(Vec::new());
@@ -30,14 +32,33 @@ fn main() -> std::io::Result<()> {
                     let str: String = chars.into_iter().collect();
                     columns.push(str);
                 }
+                column_chars.clear();
                 let row_caches: HashMap<usize, u64> = populate_hashmap(&rows);
                 let column_caches: HashMap<usize, u64> = populate_hashmap(&columns);
-                let mut row_above = 0;
+                let mut rows_above = 0;
                 let mut columns_left = 0;
-                println!("{:?}", row_caches);
-                println!("{:?}", column_caches);
-                //empty line
-                //process mirrors
+                let len = rows.len();
+                for i in 1..len {
+                    println!("Mirror position: {}", i);
+                    let depth = min(i, len - i);
+                    println!("Depth: {} {} {}", depth, i, len - i);
+                    let (_before, _after) = rows.split_at(i);
+                    let mut is_mirror = true;
+                    for j in 0..depth {
+                        println!(
+                            "{} {}",
+                            row_caches.get(&(i - j - 1)).expect("out of bounds before"),
+                            row_caches.get(&(i + j)).expect("out of bound after")
+                        );
+                        if row_caches.get(&(i - j - 1)) != row_caches.get(&(i + j)) {
+                            is_mirror = false;
+                        }
+                        //if row_caches.get[i - j] == row_caches.get[i + j];
+                    }
+                    println!("{}", is_mirror);
+                }
+                columns.clear();
+                rows.clear();
             }
         }
     }
